ba2b15aaf0e41bb447c2a09627dd7057d65c7141
Paul Chesa <paulchesa1@gmail.com>
2023-02-11 13:30:45 +0300

    Initil commit

---
+++ apps/mbaozetu-dashboard/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cows-will-fly-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'mbaozetu-dashboard';
+}
