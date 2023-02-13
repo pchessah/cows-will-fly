8beea01621ad93f779530bcf8ac5c752670ffe04
Paul Chesa <paulchesa1@gmail.com>
2023-02-13 06:58:06 +0300

    Added banner component

--- libs/elements/common-ui/src/lib/elements-common-ui.module.ts
+++ libs/elements/common-ui/src/lib/elements-common-ui.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularMaterialModule} from '@cows-will-fly/elements/external';
+
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   imports: [CommonModule, AngularMaterialModule],
-  declarations: [NavbarComponent],
-  exports:[NavbarComponent]
+  declarations: [NavbarComponent, BannerComponent],
+  exports:[NavbarComponent, BannerComponent]
 })
 export class ElementsCommonUiModule {}
