304651252155a924edf0f6d8236ed7d19d20c8cd
Paul Chesa <paulchesa1@gmail.com>
2023-02-15 04:55:20 +0300

    Added route to single product page

--- libs/features/products/src/lib/products.router.module.ts
+++ libs/features/products/src/lib/products.router.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { Routes, RouterModule } from '@angular/router';
 import { AllProductsPageComponent } from './pages/all-products/all-products.page.component';
+import { SingleProductPageComponent } from './pages/single-product-page/single-product.page.component';
 
 
 const routes: Routes = [
-  { path: '', component:AllProductsPageComponent }
+  { path: '', component:AllProductsPageComponent },
+  { path: ':id', component: SingleProductPageComponent}
 ]; 
 
 // configures NgModule imports and exports
