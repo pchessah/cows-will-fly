9a4e53a33aa9831251baeed0c0fa0ce707301fab
Paul Chesa <paulchesa1@gmail.com>
2023-02-15 04:54:58 +0300

    Added component for single product page

---
+++ libs/features/products/src/lib/pages/single-product-page/single-product.page.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-product',
+  templateUrl: 'single-product.page.component.html',
+  styleUrls: ['./single-product.page.component.scss']
+})
+
+export class SingleProductPageComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
