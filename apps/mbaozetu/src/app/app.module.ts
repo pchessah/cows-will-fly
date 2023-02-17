8f3667980075c0f82e2aa6ac69e5fd5b3ae532a3
Paul Chesa <paulchesa1@gmail.com>
2023-02-17 05:01:54 +0300

    Added error404 page component

--- apps/mbaozetu/src/app/app.module.ts
+++ apps/mbaozetu/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';  
+import { MatButtonModule } from '@angular/material/button';
 
 import { FeaturesHomeModule} from '@cows-will-fly/features/home'
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from '../routes/app.routing.module';
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, FeaturesHomeModule],
+  declarations: [AppComponent, Error404Component],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, FeaturesHomeModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- apps/mbaozetu/src/routes/app.routing.module.ts
+++ apps/mbaozetu/src/routes/app.routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router"; // CLI imports router
+import { Error404Component } from "../app/error404/error404.component";
 
 const routes: Routes = [
   {
@@ -24,6 +25,7 @@ const routes: Routes = [
       ).then((m) => m.FeaturesCartModule),
   },
   { path: "", redirectTo: "/home", pathMatch: "full" },
+  {path: "**", component:Error404Component}
 ]; 
 
 
