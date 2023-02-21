c2a63602914fc23c02f4db9d046c70ae9cde9898
Paul Chesa <paulchesa1@gmail.com>
2023-02-21 05:25:29 +0300

    Finish UI for once order has been made

--- libs/features/checkout/src/lib/pages/checkout-page/checkout.page.component.ts
+++ libs/features/checkout/src/lib/pages/checkout-page/checkout.page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class CheckOutPageComponent implements OnInit {
-  constructor() { }
+
+  orderIsPending: boolean = true;
+
+  constructor(private _router:Router) { }
 
   ngOnInit() { }
+
+  goToHome(){
+    this._router.navigateByUrl('/');
+  }
 }
