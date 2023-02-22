4e018d55db2132269a5c8d7034a7375c7700402d
Paul Chesa <paulchesa1@gmail.com>
2023-02-22 05:39:21 +0300

    Import angular fire modules

--- apps/mbaozetu/src/app/app.module.ts
+++ apps/mbaozetu/src/app/app.module.ts
@@ -2,16 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';  
 import { MatButtonModule } from '@angular/material/button';
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireModule } from "@angular/fire/compat";
 
-import { FeaturesHomeModule} from '@cows-will-fly/features/home'
+import { FeaturesHomeModule} from '@cows-will-fly/features/home';
+import { StateProductsModule } from '@cows-will-fly/state/products';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from '../routes/app.routing.module';
 import { Error404Component } from './error404/error404.component';
+import { environemnt } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, Error404Component],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, FeaturesHomeModule, MatButtonModule],
+  imports: [
+    BrowserModule, 
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FeaturesHomeModule,
+    MatButtonModule,
+    AngularFireModule.initializeApp(environemnt.firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule,
+    StateProductsModule.forRoot()
+   ],
   providers: [],
   bootstrap: [AppComponent],
 })
