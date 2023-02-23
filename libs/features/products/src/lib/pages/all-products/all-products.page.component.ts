a23ff00becb6c9b64e6031661525833752b3ab33
Paul Chesa <paulchesa1@gmail.com>
2023-02-23 21:48:10 +0300

    Fetch products from products service

--- libs/features/products/src/lib/pages/all-products/all-products.page.component.ts
+++ libs/features/products/src/lib/pages/all-products/all-products.page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from '@cows-will-fly/interfaces/product';
+import { ProductService} from '@cows-will-fly/state/products'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-all-products',
@@ -8,11 +11,12 @@ import { Component, OnInit } from '@angular/core';
 
 export class AllProductsPageComponent implements OnInit {
 
-productCount: any[] = []
+
+products$!: Observable<IProduct[]>
   
-  constructor() {
-    this.productCount = new Array(50);
+  constructor(private _productService: ProductService) {
+    this.products$ = _productService.products$
    }
 
-  ngOnInit() { }
+  ngOnInit() {}
 }
