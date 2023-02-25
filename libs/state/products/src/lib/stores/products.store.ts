e96a8999f3fd92a1c23bfd433aeee5d557ae6bd8
Paul Chesa <paulchesa1@gmail.com>
2023-02-25 12:31:10 +0300

    Update store to get data #3

--- libs/state/products/src/lib/stores/products.store.ts
+++ libs/state/products/src/lib/stores/products.store.ts
@@ -1,30 +1,21 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { BehaviorSubject } from 'rxjs';
 
-import { IProduct} from '@cows-will-fly/interfaces/product';
 
 @Injectable({providedIn: 'root'})
 
 //TODO: Add way to catch Errors
 export class ProductStore {
 
-  private _productsSrc$$ = new BehaviorSubject<IProduct[]>(null as any);
-  private _products$ = this._productsSrc$$.asObservable();
+  constructor(private _afs:AngularFirestore) { }
 
-  constructor(private _afs:AngularFirestore) {
-    this._initializedDataFromFirestore();
-   }
-
-
-  private _initializedDataFromFirestore(){
-    return this._afs.collection('products').valueChanges().subscribe(data => {
-      this._productsSrc$$.next(data as any);
-    })
-  }
 
   getProducts(){
-    return this._products$;
+    return  this._afs.collection('products').valueChanges();
+  }
+
+  getOneProduct(id:string){
+   return this._afs.collection('products').doc(id).valueChanges();
   }
 
 
