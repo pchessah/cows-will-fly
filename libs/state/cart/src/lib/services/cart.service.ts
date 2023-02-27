41ad86024feecc90accc45ee66e1dcfadf1bd2e6
Paul Chesa <paulchesa1@gmail.com>
2023-02-27 05:05:32 +0300

    Add function to update cart

--- libs/state/cart/src/lib/services/cart.service.ts
+++ libs/state/cart/src/lib/services/cart.service.ts
@@ -9,7 +9,6 @@ import { IProduct } from '@cows-will-fly/interfaces/product';
 export class CartService {
 
   private _cartSrc$$ = new BehaviorSubject<ICart[]>([]);
-
   private _cart$ =  this._cartSrc$$.asObservable();
   
   constructor() { }
@@ -20,39 +19,43 @@ export class CartService {
 
   addItemToCart(item: IProduct){
     const currentCart = this._cartSrc$$.value;
+    let updatedCart:ICart[] = [];
     const itemExists = currentCart.find(c => c.product.id === item.id);
     if(itemExists){
       const updatedItem = __CloneDeep(itemExists);
       updatedItem.count++;
-      let updatedCart = currentCart.filter(c=> c.product.id !== item.id);
+      updatedCart = currentCart.filter(c=> c.product.id !== item.id);
       updatedCart = [...updatedCart, updatedItem]
-       this._cartSrc$$.next(updatedCart);
     }  else {
       const cartItem = {product: item, count: 1}
-      const updatedCart = [...currentCart, cartItem]
-      this._cartSrc$$.next(updatedCart);
+      updatedCart = [...currentCart, cartItem]
    }
+   this.updateCart(updatedCart);
   }
 
   removeItemFromCart(item: IProduct){
     const currentCart = this._cartSrc$$.value;
+    let updatedCart:ICart[] = [];
     const itemToRemoveFromCart = currentCart.find(c => c.product.id === item.id);
     if(itemToRemoveFromCart){
       const countOfItem = itemToRemoveFromCart.count;
       if(countOfItem > 1){
         itemToRemoveFromCart.count--;
-        let updatedCart = currentCart.filter(c=> c.product.id !== item.id);
-        updatedCart = [...updatedCart, itemToRemoveFromCart]
-        this._cartSrc$$.next(updatedCart);
+        updatedCart = currentCart.filter(c=> c.product.id !== item.id);
+        updatedCart = [...updatedCart, itemToRemoveFromCart];
       } else {
-        const updatedCart = currentCart.filter(c => c.product.id !== item.id);
-        this._cartSrc$$.next(updatedCart);
+        updatedCart = currentCart.filter(c => c.product.id !== item.id);
       }
+      this.updateCart(updatedCart);
     } 
   }
 
   getCartNumber(){
     return this._cart$.pipe(map(cart => cart.length))
+  }
+
+  updateCart(updatedCart:ICart[]){
+    this._cartSrc$$.next(updatedCart);
   }
   
 }
