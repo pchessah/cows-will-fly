171b8e38b5257d9656f6de1300893afbf345b0a9
Paul Chesa <paulchesa1@gmail.com>
2023-02-28 05:19:49 +0300

    Get correct cart info #3

--- libs/features/cart/src/lib/components/cart-summary/cart-summary.component.ts
+++ libs/features/cart/src/lib/components/cart-summary/cart-summary.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { SubSink } from 'subsink';
+import { ICart } from '@cows-will-fly/interfaces/cart';
+import { CartService } from '@cows-will-fly/state/cart';
+import { IProduct } from '@cows-will-fly/interfaces/product';
 
 @Component({
   selector: 'app-cart-summary',
@@ -7,22 +11,27 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 
 export class CartSummaryComponent implements OnInit {
+  private _sbS = new SubSink();
+
   @Input()isSingleProductPage: boolean = false;
 
   displayedColumns: string[] = ['name', 'quantity', 'amount', 'remove'];
-  dataSource = products;
+  dataSource: ICart[] = [];
 
-  constructor() { }
+  constructor(private _cartService: CartService) { }
 
   ngOnInit() { 
-    this.displayedColumns = this.isSingleProductPage ? ['name','remove'] : ['name', 'quantity', 'amount', 'remove']
+    this.displayedColumns = this.isSingleProductPage ? ['name','remove'] : ['name', 'quantity', 'amount', 'remove'];
+    this._sbS.sink = 
+        this._cartService.getCart().subscribe(cart => this.dataSource = cart);
+  }
+
+  removeItemFromCart(item:IProduct){
+    this._cartService.removeItemFromCart(item);
+  }
+
+  ngOnDestroy(){
+    this._sbS.unsubscribe();
   }
 
 }
-
-
-const products: any[] = [
-  {quantity: 1, name: 'Chair', amount: 12},
-  {quantity: 2, name: 'Table', amount: 130},
-  {quantity: 3, name: 'Desk', amount: 10}
-];
