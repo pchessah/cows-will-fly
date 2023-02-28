6a687e987e2216a815ef0f60e03b126a05a9783e
Paul Chesa <paulchesa1@gmail.com>
2023-02-28 05:24:23 +0300

    update cart

--- libs/features/cart/src/lib/pages/cart/cart.page.component.ts
+++ libs/features/cart/src/lib/pages/cart/cart.page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CartService } from '@cows-will-fly/state/cart';
+import { Observable, map } from 'rxjs';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-cart-page',
@@ -9,18 +12,25 @@ import { Router } from '@angular/router';
 
 export class CartPageComponent implements OnInit {
 
+  private _sbS = new SubSink();
   noItemsInCart: boolean = false;
 
-  constructor(private _router:Router) { }
+  constructor(private _router:Router,
+              private _cartService:CartService) { }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    this._sbS.sink = 
+      this._cartService.getCartNumber().pipe(map(n =>  n === 0 )).subscribe(bool =>{
+        this.noItemsInCart = bool;
+      });
+  }
 
   goToProducts(){
     this._router.navigateByUrl('/products');
   }
 
   clearCart(){
-    this.noItemsInCart = true;
+    this._cartService.clearCart();
   }
 
   goToCheckout(){
