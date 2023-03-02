393a7601281c489127c87b416f883ae62669f62c
Paul Chesa <paulchesa1@gmail.com>
2023-03-02 08:20:14 +0300

    Add functionality to check if there is something in the cart #3

--- libs/features/checkout/src/lib/pages/checkout-page/checkout.page.component.ts
+++ libs/features/checkout/src/lib/pages/checkout-page/checkout.page.component.ts
@@ -1,21 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { map }   from "rxjs";
+import { Router }            from "@angular/router";
+import { CartService }       from "@cows-will-fly/state/cart";
+import { SubSink } from "subsink";
 
 @Component({
-  selector: 'app-checkout',
-  templateUrl: 'checkout.page.component.html',
-  styleUrls: ['./checkout.page.component.scss']
+  selector: "app-checkout",
+  templateUrl: "checkout.page.component.html",
+  styleUrls: ["./checkout.page.component.scss"],
 })
 
 export class CheckOutPageComponent implements OnInit {
-
+  private _sbS = new SubSink()
   orderIsPending: boolean = true;
+  noItemsInCart: boolean = false;
 
-  constructor(private _router:Router) { }
+  constructor(private _router: Router, private _cartService: CartService) {}
 
-  ngOnInit() { }
-
-  goToHome(){
-    this._router.navigateByUrl('/');
+  ngOnInit() {
+    this._sbS.sink = 
+        this._cartService.getCartNumber().pipe(map(n =>  n === 0 )).subscribe(bool =>{
+          this.noItemsInCart = bool;
+        });
   }
+  goToHome() {
+    this._router.navigateByUrl("/");
+  }
+
+  goToProducts(){
+    this._router.navigateByUrl('/products');
+  }
+
+  ngOnDestroy(){
+    this._sbS.unsubscribe();
+  }
+
 }
