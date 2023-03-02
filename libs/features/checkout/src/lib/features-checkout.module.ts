cc4806f348454d40e4f8eea71e285c30b5677a1c
Paul Chesa <paulchesa1@gmail.com>
2023-03-02 08:20:59 +0300

    Import cart module in checkout module #3

--- libs/features/checkout/src/lib/features-checkout.module.ts
+++ libs/features/checkout/src/lib/features-checkout.module.ts
@@ -1,14 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from                         '@angular/core';
+import { CommonModule } from                     '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AngularMaterialModule} from '@cows-will-fly/elements/external';
-import { ElementsCommonUiModule} from '@cows-will-fly/elements/common-ui';
-import { CheckOutPageComponent } from './pages/checkout-page/checkout.page.component';
-import { CheckOutRoutingModule } from './checkout.routing.module';
-import { CheckoutFormComponent } from './components/checkout-form/checkout-form.component';
+import { FeaturesCartModule } from               '@cows-will-fly/features/cart';
+import { AngularMaterialModule} from             '@cows-will-fly/elements/external';
+import { ElementsCommonUiModule} from            '@cows-will-fly/elements/common-ui';
+import { CheckOutPageComponent } from            './pages/checkout-page/checkout.page.component';
+import { CheckOutRoutingModule } from            './checkout.routing.module';
+import { CheckoutFormComponent } from            './components/checkout-form/checkout-form.component';
 
 @NgModule({
-  imports: [CommonModule,FormsModule, ReactiveFormsModule,CheckOutRoutingModule, AngularMaterialModule, ElementsCommonUiModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CheckOutRoutingModule,
+    AngularMaterialModule,
+    ElementsCommonUiModule,
+    FeaturesCartModule
+    
+  ],
   declarations:[CheckOutPageComponent, CheckoutFormComponent]
 })
 
