934a5d5e11e765a0c9f6e7c3ee50ceaf284acbde
Paul Chesa <paulchesa1@gmail.com>
2023-03-06 22:29:35 +0300

    Added function to add order to firestore #3

--- libs/state/checkout/src/lib/services/checkout.service.ts
+++ libs/state/checkout/src/lib/services/checkout.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable }       from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { from }             from 'rxjs';
+
+import {IUserDetails }      from "@cows-will-fly/interfaces/user";
+import {ICart, IOrder}      from '@cows-will-fly/interfaces/cart';
+
 
 @Injectable({providedIn: 'root'})
 
 
 export class CheckoutStateService {
-  constructor() { }
+
+  constructor(private _afs:AngularFirestore) { }
+
+  createOrder(cart:ICart[], userDetails:IUserDetails){
+    const id = this._afs.createId();
+    const order: IOrder = {
+      cart: cart,
+      user:userDetails,
+      id: id
+    }
+    return from(this._afs.doc(`orders/${id}`).set(order));
+  }
   
 }
