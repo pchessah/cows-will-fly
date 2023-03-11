bed74c599593638f508d0a02e6386bc4bfd6d9e3
Paul Chesa <paulchesa1@gmail.com>
2023-03-11 21:53:43 +0300

    Add components for auth

--- libs/features/auth/src/lib/features-auth.module.ts
+++ libs/features/auth/src/lib/features-auth.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule }                from '@angular/core';
+import { CommonModule }            from '@angular/common';
+import { ElementsExternalModule }  from '@cows-will-fly/elements/external';
+import { SignUpComponent }         from './components/signup/signup.component';
+import { LogInComponent }          from './components/login/login.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { AuthRoutingModule }       from './auth.routing.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ElementsExternalModule, AuthRoutingModule],
+  declarations:[SignUpComponent, LogInComponent, ForgotPasswordComponent],
+  exports:[SignUpComponent, LogInComponent, ForgotPasswordComponent]
 })
 export class FeaturesAuthModule {}
