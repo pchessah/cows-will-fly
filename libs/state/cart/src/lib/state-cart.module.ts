a6aa9efde41c2f9ace8fc414cc9012db5a3b8e9c
Paul Chesa <paulchesa1@gmail.com>
2023-03-11 21:25:20 +0300

    Add snackbar when doing cart actions

--- libs/state/cart/src/lib/state-cart.module.ts
+++ libs/state/cart/src/lib/state-cart.module.ts
@@ -1,9 +1,10 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CartService } from './services/cart.service';
+import { CommonModule }                  from '@angular/common';
+import { MatSnackBarModule }             from '@angular/material/snack-bar';
+import { CartService }                   from './services/cart.service';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatSnackBarModule],
 })
 export class StateCartModule {
   static forRoot(): ModuleWithProviders<StateCartModule> {
