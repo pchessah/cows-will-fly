788143c38bb987e4646cbd329c88f81407779b5a
Paul Chesa <paulchesa1@gmail.com>
2023-03-11 21:54:35 +0300

    Created auth routing module

---
+++ libs/features/auth/src/lib/auth.routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'; 
+import { LogInComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/signup/signup.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+
+
+const routes: Routes = [
+  { path: 'login', component:LogInComponent },
+  { path: 'signup', component:SignUpComponent},
+  { path: 'forgot-password', component:ForgotPasswordComponent},
+  { path: "", redirectTo: "/login", pathMatch: "full" }
+]; 
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
