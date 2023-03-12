ed287ff8a4ad8721ef840a62f0e23e1d23c54b38
Paul Chesa <paulchesa1@gmail.com>
2023-03-12 15:28:48 +0300

    Add encryption/decryption for local storage

--- libs/state/local-storage/src/lib/services/local-storage.service.ts
+++ libs/state/local-storage/src/lib/services/local-storage.service.ts
@@ -1,25 +1,37 @@
 import { Injectable } from '@angular/core';
+import * as CryptoJS from "crypto-js";
+
 
 @Injectable({providedIn: 'root'})
 
 export class LocalStorageService {
 
-  //TODO: IMPLEMENT CRYPTO JS
+  key = 'cows-will-fly'
+
   constructor() { }
 
   public saveData(key: string, value: string) {
-    localStorage.setItem(key, value);
+    return localStorage.setItem(key, value);
   }
 
   public getData(key: string) {
-    return localStorage.getItem(key)
+    return localStorage.getItem(key);
   }
+
   public removeData(key: string) {
     localStorage.removeItem(key);
   }
 
   public clearData() {
     localStorage.clear();
+  }
+
+  private encrypt(txt: string): string {
+    return CryptoJS.AES.encrypt(txt, this.key).toString();
+  }
+
+  private decrypt(txtToDecrypt: string) {
+    return CryptoJS.AES.decrypt(txtToDecrypt, this.key).toString(CryptoJS.enc.Utf8);
   }
   
 }
