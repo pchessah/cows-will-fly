c51269f5ef1b686895e6fcf64c5b2074b03a9d9e
Paul Chesa <paulchesa1@gmail.com>
2023-03-12 15:28:11 +0300

    Add fns to sign up and sign in

--- libs/features/auth/src/lib/components/login/login.component.ts
+++ libs/features/auth/src/lib/components/login/login.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }                  from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router }                             from '@angular/router';
+import { SubSink }                            from 'subsink';
+import { AuthService }                        from '@cows-will-fly/state/auth';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +14,10 @@ export class LogInComponent implements OnInit {
 
   loginForm:FormGroup;
 
-  constructor(private _fb:FormBuilder) {
+  private _sbs = new SubSink();
+  isLoading: boolean = false;
+
+  constructor(private _fb:FormBuilder, private _router:Router, private _authService:AuthService) {
     this.loginForm = this._fb.group({
       email: ['' , [Validators.email, Validators.required]],
       password: ['' , [ Validators.required]]
@@ -22,7 +28,23 @@ export class LogInComponent implements OnInit {
 
   submit() {
     if (this.loginForm.valid) {
-     // this.submitEM.emit(this.form.value);
+      this.isLoading = true;
+      const email = this.loginForm.value.email;
+      const password = this.loginForm.value.password;
+      this._sbs.sink = 
+          this._authService.signIn(email, password).subscribe(res =>{
+            this.isLoading = false
+
+          })
     }
   }
+
+  goToSignUp(){
+    this._router.navigateByUrl('/auth/signup');
+  }
+
+  goToForgotPassword(){
+    this._router.navigateByUrl('/auth/forgot-password');
+  }
+
 }
--- libs/features/auth/src/lib/components/signup/signup.component.ts
+++ libs/features/auth/src/lib/components/signup/signup.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit }       from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router }                             from '@angular/router';
+import { SubSink }                            from "subsink";
+import { AuthService }                        from '@cows-will-fly/state/auth';
 
 @Component({
   selector: 'app-signup',
@@ -7,24 +10,62 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls:['signup.component.scss']
 })
 
-export class SignUpComponent implements OnInit {
+export class SignUpComponent implements OnInit, OnDestroy{
   
+  private _sbS = new SubSink();
   signupForm: FormGroup;
+  isPasswordTheSame:boolean = true;
+  isLoading: boolean = false;
+  
+  constructor(private _fb:FormBuilder,
+              private _authService: AuthService,
+              private _router:Router) {
 
-  constructor(private _fb:FormBuilder) {
     this.signupForm = this._fb.group({
       email: ['' , [Validators.email, Validators.required]],
-      password: ['' , [ Validators.required]],
-      password2: ['' , [ Validators.required]]
+      password: ['' , [ Validators.required, Validators.minLength(8)]],
+      password2: ['' , [ Validators.required, Validators.minLength(8)]]
     })
   }
 
 
-  ngOnInit() { }
+  ngOnInit() { 
+    this._sbS.sink = 
+        this.signupForm.valueChanges.subscribe(val =>{
+          const password1 = val.password;
+          const password2 = val.password2;
+          this.isPasswordTheSame = password1 === password2
+        });
+  }
 
   submit() {
-    if (this.signupForm.valid) {
-     // this.submitEM.emit(this.form.value);
+    if (this.signupForm.valid && this.isPasswordTheSame) {
+      this.isLoading = true;
+      const signUpFormValue = this.signupForm.value;
+      const email = signUpFormValue.email;
+      const password = signUpFormValue.password;
+      this._sbS.sink =
+          this._authService.signUp(email, password).subscribe(res =>{
+            this.isLoading = false;
+            this.goToLogin();
+          });
     }
   }
+
+  goToLogin(){
+    this._router.navigateByUrl('/auth/login');
+  }
+
+  goToForgotPassword(){
+    this._router.navigateByUrl('/auth/forgot-password');
+  }
+
+  check(val:string){
+    return this.signupForm.get(val)?.invalid;
+  }
+
+  ngOnDestroy(): void {
+    this._sbS.unsubscribe();
+  }
+
 }
