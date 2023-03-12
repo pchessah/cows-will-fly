10c54791e1fdeae10968e5bf8d303b2bd52c12f1
Paul Chesa <paulchesa1@gmail.com>
2023-03-12 15:46:51 +0300

    Finish forgot password

--- libs/features/auth/src/lib/components/forgot-password/forgot-password.component.ts
+++ libs/features/auth/src/lib/components/forgot-password/forgot-password.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }                  from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router }                             from '@angular/router';
+import { AuthService } from '@cows-will-fly/state/auth';
+import { SubSink }                            from 'subsink';
 
 @Component({
   selector: 'app-forgot-password',
@@ -9,9 +12,14 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 export class ForgotPasswordComponent implements OnInit {
   
-  forgotPasswordForm:FormGroup;
+  private _sBs = new SubSink();
 
-  constructor(private _fb:FormBuilder) {
+  forgotPasswordForm:FormGroup;
+  isLoading: boolean = false;
+
+  constructor(private _fb:FormBuilder,
+              private _authService: AuthService,
+              private _router:Router) {
     this.forgotPasswordForm = this._fb.group({
       email: ['' , [Validators.email, Validators.required]]
     })
@@ -21,7 +29,24 @@ export class ForgotPasswordComponent implements OnInit {
 
   submit() {
     if (this.forgotPasswordForm.valid) {
-     // this.submitEM.emit(this.form.value);
+      this.isLoading = true;
+      const email = this.forgotPasswordForm.value.email;
+      this._sBs.sink = 
+          this._authService.forgotPassword(email).subscribe((res)=>{
+            this.goToLogin();
+            this.isLoading = false;
+          })
     }
   }
+
+  goToLogin(){
+    this._router.navigateByUrl('/auth/login');
+  }
+
+  goToSignUp(){
+    this._router.navigateByUrl('/auth/signup');
+  }
+
+
+
 }
