ca567936aab4ed54c0e0fe8a4813297488ae28f9
Paul Chesa <paulchesa1@gmail.com>
2023-03-12 08:09:15 +0300

    Finish design of sign up form

--- libs/features/auth/src/lib/components/signup/signup.component.ts
+++ libs/features/auth/src/lib/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -8,7 +9,22 @@ import { Component, OnInit } from '@angular/core';
 
 export class SignUpComponent implements OnInit {
   
-  constructor() { }
+  signupForm: FormGroup;
+
+  constructor(private _fb:FormBuilder) {
+    this.signupForm = this._fb.group({
+      email: ['' , [Validators.email, Validators.required]],
+      password: ['' , [ Validators.required]],
+      password2: ['' , [ Validators.required]]
+    })
+  }
+
 
   ngOnInit() { }
+
+  submit() {
+    if (this.signupForm.valid) {
+     // this.submitEM.emit(this.form.value);
+    }
+  }
 }
