30230e483ded032b02f0827e0429460317b6fd55
Paul Chesa <paulchesa1@gmail.com>
2023-03-22 09:09:18 +0300

    Update user details interface to have delivery details

--- libs/interfaces/user/src/lib/user-details.interface.ts
+++ libs/interfaces/user/src/lib/user-details.interface.ts
@@ -2,4 +2,12 @@ export interface IUserDetails {
   phone:number;
   email:string;
   name:string;
+  location?:IDeliveryLocation;
+  deliveryDetails?:string;
+
+}
+
+export interface IDeliveryLocation {
+  name: string;
+  cost: number;
 }
