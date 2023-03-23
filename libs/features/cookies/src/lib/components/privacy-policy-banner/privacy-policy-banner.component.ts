e2a99c37eefebeabbff9a0671ad0b252cbcb7089
Paul Chesa <paulchesa1@gmail.com>
2023-03-23 05:20:28 +0300

    :tada: Created component for privacy policy banner

---
+++ libs/features/cookies/src/lib/components/privacy-policy-banner/privacy-policy-banner.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-privacy-policy-banner',
+  templateUrl: 'privacy-policy-banner.component.html',
+  styleUrls: ['privacy-policy-banner.component.scss']
+})
+
+export class PrivacyPolicyBannerComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit() { }
+}
--- libs/features/cookies/src/lib/features-cookies.module.ts
+++ libs/features/cookies/src/lib/features-cookies.module.ts
@@ -1,11 +1,15 @@
-import { NgModule }              from '@angular/core';
-import { CommonModule }          from '@angular/common';
-import { ElementsExternalModule }from '@cows-will-fly/elements/external';
-import { ElementsCommonUiModule }from '@cows-will-fly/elements/common-ui';
-import { CookiesRoutingModule }  from './cookies.routing.module';
+import { NgModule }                     from '@angular/core';
+import { CommonModule }                 from '@angular/common';
+import { ElementsExternalModule }       from '@cows-will-fly/elements/external';
+import { ElementsCommonUiModule }       from '@cows-will-fly/elements/common-ui';
+import { CookiesRoutingModule }         from './cookies.routing.module';
+import { PrivacyPolicyBannerComponent } from './components/privacy-policy-banner/privacy-policy-banner.component';
+import { PrivacyPolicyPageComponent }   from './pages/privacy-policy/privacy-policy.page.component';
 
 @NgModule({
   imports: [CommonModule, ElementsExternalModule, ElementsCommonUiModule,
             CookiesRoutingModule],
+  declarations: [PrivacyPolicyBannerComponent, PrivacyPolicyPageComponent],
+  exports: [PrivacyPolicyBannerComponent]
 })
 export class FeaturesCookiesModule {}
