c3ec5a9d6fcb71aec03ee79cdbd1c74f033963bb
Paul Chesa <paulchesa1@gmail.com>
2023-03-23 05:04:57 +0300

    Created privacy policy page

---
+++ libs/features/cookies/src/lib/pages/privacy-policy/privacy-policy.page.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-privacy-policy',
+  templateUrl: 'privacy-policy.page.component.html',
+  styleUrls: ['privacy-policy.page.component.scss']
+})
+
+export class PrivacyPolicyComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
