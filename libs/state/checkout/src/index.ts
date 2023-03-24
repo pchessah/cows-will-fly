4a39795bcd79c802f6019013c48f24a12c72411f
Paul Chesa <paulchesa1@gmail.com>
2023-03-24 15:51:22 +0300

    Export promo code service

--- libs/state/checkout/src/index.ts
+++ libs/state/checkout/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/state-checkout.module';
 export * from './lib/services/checkout.service';
 export * from './lib/services/email-checkout.service';
+export * from './lib/services/promocodes.service';
