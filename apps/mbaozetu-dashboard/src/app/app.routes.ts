34ff259ad935c82a0f6d30ae5d8c07f3e85f22ab
Paul Chesa <paulchesa1@gmail.com>
2023-03-27 09:11:56 +0300

    :sparkles: Added routes for dashboard

--- apps/mbaozetu-dashboard/src/app/app.routes.ts
+++ apps/mbaozetu-dashboard/src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Route } from '@angular/router';
+import { Route } from "@angular/router";
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+
+  //Auth module
+  {
+    path: "auth",
+    loadChildren: () =>
+      import(
+        "../../../../libs/features/auth/src/lib/features-auth.module"
+      ).then((m) => m.FeaturesAuthModule),
+  },
+
+  //Home module
+  {
+    path: "home",
+    loadChildren: () =>
+      import(
+        "../../../../libs/features/home/src/lib/features-home.module"
+      ).then((m) => m.FeaturesHomeModule),
+  },
+
+  { path: "", redirectTo: "/home/dashboard", pathMatch: "full" }, //Home route
+];
