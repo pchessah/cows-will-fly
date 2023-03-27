b15ab51fbd200e42db4a250fa6d9bcf0a16b5709
Paul Chesa <paulchesa1@gmail.com>
2023-03-27 09:09:51 +0300

    :sparkles: Export angular material

--- libs/elements/common-ui/src/lib/elements-common-ui.module.ts
+++ libs/elements/common-ui/src/lib/elements-common-ui.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule }             from '@angular/core';
+import { CommonModule }         from '@angular/common';
 import { AngularMaterialModule} from '@cows-will-fly/elements/external';
-
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { BannerComponent } from './components/banner/banner.component';
+import { NavbarComponent }      from './components/navbar/navbar.component';
+import { BannerComponent }      from './components/banner/banner.component';
 
 @NgModule({
   imports: [CommonModule, AngularMaterialModule],
+
   declarations: [NavbarComponent, BannerComponent],
-  exports:[NavbarComponent, BannerComponent]
+  
+  exports:[NavbarComponent, BannerComponent, AngularMaterialModule]
 })
 export class ElementsCommonUiModule {}
