cc897e3a3b1aa2018fac9030b4b75dfb263b88c4
Paul Chesa <paulchesa1@gmail.com>
2023-03-27 09:12:22 +0300

    :sparkles: Added firestore modules

--- apps/mbaozetu-dashboard/src/app/app.module.ts
+++ apps/mbaozetu-dashboard/src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
-import { appRoutes } from './app.routes';
+import { NgModule }                 from '@angular/core';
+import { BrowserModule }            from '@angular/platform-browser';
+import { RouterModule }             from '@angular/router';
+import { BrowserAnimationsModule }  from '@angular/platform-browser/animations';  
+import { AngularFireAuthModule }    from "@angular/fire/compat/auth";
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule }   from '@angular/fire/compat/firestore';
+import { AngularFireModule }        from "@angular/fire/compat";
+import { ElementsCommonUiModule }   from '@cows-will-fly/elements/common-ui';
+import { environemnt }              from '../environments/environment';
+import { AppComponent }             from './app.component';
+import { appRoutes }                from './app.routes';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environemnt.firebaseConfig),
+    AngularFireModule,
+    AngularFirestoreModule.enablePersistence(),
+    AngularFireStorageModule,
+    AngularFireAuthModule,
+    ElementsCommonUiModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+
   ],
   providers: [],
   bootstrap: [AppComponent],
