81abc4523f3b93e74934048c69da6da608a4e3c1
Paul Chesa <paulchesa1@gmail.com>
2023-03-28 05:18:11 +0300

    :tada: :construction: Added products table component

---
+++ libs/features/products/src/lib/components/products-table/products-table.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-table',
+  templateUrl: 'products-table.component.html',
+  styleUrls: ['products-table.component.scss']
+})
+
+export class ProductsTableComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
