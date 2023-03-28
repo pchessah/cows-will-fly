b5b93f6ef9ba28d65087efe26284683963c028bb
Paul Chesa <paulchesa1@gmail.com>
2023-03-28 05:20:21 +0300

    :tada: Created dashboard chart component

--- libs/features/home/src/lib/features-home.module.ts
+++ libs/features/home/src/lib/features-home.module.ts
@@ -1,17 +1,18 @@
-import { NgModule }               from '@angular/core';
-import { CommonModule }           from '@angular/common';
-import { AngularMaterialModule}   from '@cows-will-fly/elements/external';
-import { ElementsCommonUiModule}  from '@cows-will-fly/elements/common-ui';
-import { HomeRoutingModule }      from './home.routing.module';
-import { HomePageComponent }      from './pages/home/home.page';
-import { DashboardPageComponent } from './pages/dashboard/dashboard.page.component';
+import { NgModule }                from '@angular/core';
+import { CommonModule }            from '@angular/common';
+import { AngularMaterialModule}    from '@cows-will-fly/elements/external';
+import { ElementsCommonUiModule}   from '@cows-will-fly/elements/common-ui';
+import { HomeRoutingModule }       from './home.routing.module';
+import { HomePageComponent }       from './pages/home/home.page';
+import { DashboardPageComponent }  from './pages/dashboard/dashboard.page.component';
+import { DashboardChartComponent } from './components/dashboard-chart/dashboard-chart.component';
 
 @NgModule({
   imports: [ CommonModule, AngularMaterialModule, HomeRoutingModule,
              ElementsCommonUiModule],
 
-  declarations:[HomePageComponent, DashboardPageComponent],
+  declarations:[HomePageComponent, DashboardPageComponent, DashboardChartComponent],
   
-  exports: [HomePageComponent, DashboardPageComponent]
+  exports: [HomePageComponent, DashboardPageComponent, DashboardChartComponent]
 })
 export class FeaturesHomeModule {}
