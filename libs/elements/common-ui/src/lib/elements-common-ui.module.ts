6f986c484e9dc26ba475cab0c35fc0a766fede58
Paul Chesa <paulchesa1@gmail.com>
2023-03-28 05:17:17 +0300

    :tada: Added reusable chart component

--- libs/elements/common-ui/src/lib/elements-common-ui.module.ts
+++ libs/elements/common-ui/src/lib/elements-common-ui.module.ts
@@ -3,12 +3,16 @@ import { CommonModule }         from '@angular/common';
 import { AngularMaterialModule} from '@cows-will-fly/elements/external';
 import { NavbarComponent }      from './components/navbar/navbar.component';
 import { BannerComponent }      from './components/banner/banner.component';
+import { SideBarComponent }     from './components/sidebar/sidebar.component';
+import { ChartComponent }       from './components/chart/chart.component';
 
 @NgModule({
   imports: [CommonModule, AngularMaterialModule],
 
-  declarations: [NavbarComponent, BannerComponent],
+  declarations: [NavbarComponent, BannerComponent, SideBarComponent, ChartComponent],
   
-  exports:[NavbarComponent, BannerComponent, AngularMaterialModule]
+  exports:[ NavbarComponent, BannerComponent, SideBarComponent,
+            AngularMaterialModule, ChartComponent]
 })
+
 export class ElementsCommonUiModule {}
