8d6d5748c1b0efae014a89185f8fd79973754fd1
Paul Chesa <paulchesa1@gmail.com>
2023-03-28 10:55:51 +0300

    :sparkles: Use function to route

--- libs/elements/common-ui/src/lib/components/sidebar/sidebar.component.ts
+++ libs/elements/common-ui/src/lib/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class SideBarComponent implements OnInit {
-  constructor() { }
+  constructor(private _router:Router) { }
 
   ngOnInit() { }
+
+  goTo(url:string){
+    this._router.navigateByUrl(url)
+  }
 }
