b410f10615a13197d2ebb1b8f4148973d0df4dbc
Paul Chesa <paulchesa1@gmail.com>
2023-03-29 04:58:02 +0300

    products table started

--- libs/features/products/src/lib/components/products-table/products-table.component.ts
+++ libs/features/products/src/lib/components/products-table/products-table.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit,
+         ViewChild }            from '@angular/core';
+import {MatTableDataSource}     from '@angular/material/table';
+import {MatPaginator}           from '@angular/material/paginator';
+import { MatDialog }            from '@angular/material/dialog';
+import { Observable }           from 'rxjs';
+import { SubSink }              from 'subsink';
+import { IProduct }             from '@cows-will-fly/interfaces/product';
+import { ProductStore }         from '@cows-will-fly/state/products';
+import { ProductFormComponent } from '../product-form/product-form.component';
 
 @Component({
   selector: 'app-products-table',
@@ -6,8 +15,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['products-table.component.scss']
 })
 
-export class ProductsTableComponent implements OnInit {
-  constructor() { }
+export class ProductsTableComponent implements OnInit, AfterViewInit {
+  private _sbs = new SubSink();
+  displayedColumns: string[] = ['image', 'name', 'price', 'action'];
+  dataSource = new MatTableDataSource();
+  products$: Observable<IProduct[]> = null as any;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  ngOnInit() { }
+  constructor(private _productsStore$$: ProductStore, private _dialog:MatDialog) { }
+
+  ngOnInit() { 
+    this.products$ = this._productsStore$$.getProducts() as Observable<IProduct[]>;
+  }
+
+
+  ngAfterViewInit() {
+    this._sbs.sink = 
+    this.products$.subscribe(products =>{
+      this.dataSource.data = products;
+      this.dataSource.paginator = this.paginator;
+    })
+   
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  addProduct(){
+    this._dialog.open(ProductFormComponent, {
+      width: '550px'
+    })
+
+  }
 }
