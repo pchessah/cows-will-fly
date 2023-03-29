abfbc6c7fa6767d7e19301f4cb52dcaa12f29cef
Paul Chesa <paulchesa1@gmail.com>
2023-03-29 20:52:26 +0300

    :tada: Created orders-overview page component

---
+++ libs/features/orders/src/lib/pages/orders-overview/orders-overview.page.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-overview',
+  templateUrl: 'orders-overview.page.component.html',
+  styleUrls:['orders-overview.page.component.scss']
+})
+
+export class OrdersOverviewComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
