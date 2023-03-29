e488373baafbd90d97e9f6a9e5a1e17e46fcc23b
Paul Chesa <paulchesa1@gmail.com>
2023-03-29 20:52:50 +0300

    :tada: Created orders table component

---
+++ libs/features/orders/src/lib/components/orders-table/orders-table.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-table',
+  templateUrl: 'orders-table.component.html',
+  styleUrls: ['orders-table.component.scss']
+})
+
+export class OrdersTableComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
