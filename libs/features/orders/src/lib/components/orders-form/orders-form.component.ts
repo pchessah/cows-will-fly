6859ebd694b7a076237ae8d9db272549695359a0
Paul Chesa <paulchesa1@gmail.com>
2023-03-29 20:53:43 +0300

    :tada: created orders form component

---
+++ libs/features/orders/src/lib/components/orders-form/orders-form.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-form',
+  templateUrl: 'orders-form.component.html',
+  styleUrls: ['orders-form.component.scss']
+})
+
+export class OrdersFormComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit() { }
+}
