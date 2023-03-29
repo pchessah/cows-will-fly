d6e30f1b6c5affdf4b744af7848a58d5dabc26d5
Paul Chesa <paulchesa1@gmail.com>
2023-03-29 19:02:55 +0300

    Call functions to edit and delete products

--- libs/features/products/src/lib/components/products-table/products-table.component.ts
+++ libs/features/products/src/lib/components/products-table/products-table.component.ts
@@ -1,8 +1,9 @@
-import { AfterViewInit, Component, OnInit,
+import { OnDestroy, Component, OnInit,
          ViewChild }            from '@angular/core';
 import {MatTableDataSource}     from '@angular/material/table';
 import {MatPaginator}           from '@angular/material/paginator';
 import { MatDialog }            from '@angular/material/dialog';
+import { MatSnackBar }          from '@angular/material/snack-bar';
 import { Observable }           from 'rxjs';
 import { SubSink }              from 'subsink';
 import { IProduct }             from '@cows-will-fly/interfaces/product';
@@ -15,27 +16,23 @@ import { ProductFormComponent } from '../product-form/product-form.component';
   styleUrls: ['products-table.component.scss']
 })
 
-export class ProductsTableComponent implements OnInit, AfterViewInit {
+export class ProductsTableComponent implements OnInit, OnDestroy {
   private _sbs = new SubSink();
   displayedColumns: string[] = ['image', 'name', 'price', 'action'];
   dataSource = new MatTableDataSource();
   products$: Observable<IProduct[]> = null as any;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(private _productsStore$$: ProductStore, private _dialog:MatDialog) { }
+  constructor(private _productsStore$$: ProductStore,
+              private _dialog:MatDialog,
+              private _snackBar:MatSnackBar) { }
 
   ngOnInit() { 
     this.products$ = this._productsStore$$.getProducts() as Observable<IProduct[]>;
-  }
-
-
-  ngAfterViewInit() {
     this._sbs.sink = 
-    this.products$.subscribe(products =>{
-      this.dataSource.data = products;
-      this.dataSource.paginator = this.paginator;
-    })
-   
+        this.products$.subscribe(products =>{
+          this.dataSource.data = products;
+        })
   }
 
   applyFilter(event: Event) {
@@ -45,8 +42,33 @@ export class ProductsTableComponent implements OnInit, AfterViewInit {
 
   addProduct(){
     this._dialog.open(ProductFormComponent, {
-      width: '550px'
+      width: '550px',
+      data: {mode:'create'}
     })
+  }
 
+
+  editProduct(product:IProduct){
+    this._dialog.open(ProductFormComponent, {
+      width: '550px',
+      data: {mode:'edit', productToEdit: product}
+    })
+  }
+
+  deleteProduct(product:IProduct){
+    const confirm$ = window.confirm('Are you sure you want to delete '+ product.name+ '?');
+    if(confirm$){
+      this._sbs.sink = 
+          this._productsStore$$.deleteProduct(product).subscribe(res => {
+            this._snackBar.open("Product deleted successfully", "x",{
+              verticalPosition:'top',
+              duration: 3000
+            });
+          })
+    } 
+  }
+
+  ngOnDestroy(): void {
+    this._sbs.unsubscribe();
   }
 }
