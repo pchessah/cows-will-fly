12d5946f647f59630b71d3a6875a77b48d4bbd53
Paul Chesa <paulchesa1@gmail.com>
2023-06-12 11:23:49 +0300

    :construction: Work on the messaging module

--- apps/notify-pwa/src/app/home-screen/home-screen.component.ts
+++ apps/notify-pwa/src/app/home-screen/home-screen.component.ts
@@ -1,79 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-import { SubSink }           from 'subsink';
-import { AvailabilityService } from '@cows-will-fly/state/pwa';
+import { Component, OnInit, inject } from "@angular/core";
+import { SubSink } from "subsink";
+import { AvailabilityService } from "@cows-will-fly/state/pwa";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
-import { environemnt } from "../../environments/environment"
-import { AngularFireMessaging } from '@angular/fire/compat/messaging';
+import { environemnt } from "../../environments/environment";
+import { AngularFireMessaging } from "@angular/fire/compat/messaging";
+import { BehaviorSubject } from "rxjs";
+import { Functions } from "@angular/fire/functions";
 
 @Component({
-  selector: 'cows-will-fly-home-screen',
-  templateUrl: './home-screen.component.html',
-  styleUrls: ['./home-screen.component.css']
+  selector: "cows-will-fly-home-screen",
+  templateUrl: "./home-screen.component.html",
+  styleUrls: ["./home-screen.component.css"],
 })
 export class HomeScreenComponent implements OnInit {
-
   private _sbS = new SubSink();
-  message:any = null;
-  availability!:'available' | "travelled" | "unavailable";
+  message: any = null;
+  availability!: "available" | "travelled" | "unavailable";
+  currentMessage = new BehaviorSubject(null);
+  private functions: Functions = inject(Functions);
 
-  constructor(private _availabilityService: AvailabilityService, private _angularFireMessaging:AngularFireMessaging){
-    this._angularFireMessaging.messages.subscribe(
-      (_messaging: any) => {
-        _messaging.onMessage = _messaging.onMessage.bind(_messaging);
-        _messaging.onTokenRefresh = _messaging.onTokenRefresh.bind(_messaging);
-        debugger
-      }
-    );
-
-  }
-
-  ngOnInit(): void {
-    this._sbS.sink = this._availabilityService.getCurrentStatus().subscribe(res =>{
-      this.availability = (res[0] as any).status
+  constructor(
+    private _availabilityService: AvailabilityService,
+    private angularFireMessaging: AngularFireMessaging
+  ) {
+    this.angularFireMessaging.messages.subscribe((_messaging) => {
+      debugger;
     });
-
-    this.requestPermission();
-    // this.listen();
-    // this.backgroundMessage()
-    // this.foregroundmessage();
-
-    
   }
 
   requestPermission() {
-    this._angularFireMessaging.requestToken .subscribe(res =>{
-      debugger
-    })
+    this.angularFireMessaging.requestToken.subscribe((token) => {
+      console.log(token);
+      debugger;
+    });
   }
 
-  listen() {
-    this._angularFireMessaging.messages.subscribe((message: any) => {
-      console.log('Foreground message: ' + message)
-    })
-  }
-
-
-  backgroundMessage() {
-    this._angularFireMessaging.onBackgroundMessage((payload) => {
-      console.log('Message received. ', payload);
-      this.message=payload;
-      debugger
+  receiveMessage() {
+    this.angularFireMessaging.messages.subscribe((payload) => {
+      console.log("new message received. ", payload);
+      debugger;
     });
   }
 
 
-  foregroundmessage(){
-    this._angularFireMessaging.onMessage((payload)=>{
-      console.log('Message received. ', payload);
-      this.message=payload;
-      debugger
-    })
+  sendMessage(){
+    this.functions
   }
 
-  
+  ngOnInit(): void {
+    this._sbS.sink = this._availabilityService
+      .getCurrentStatus()
+      .subscribe((res) => {
+        this.availability = (res[0] as any).status;
+      });
 
-
-
-
-
+      this.requestPermission()
+      this.receiveMessage();
+  }
 }
--- apps/notify-pwa/src/firebase-messaging-sw.js
+++ apps/notify-pwa/src/firebase-messaging-sw.js
@@ -1,16 +1,38 @@
-// importScripts("https://www.gstatic.com/firebasejs/9.17.1/firebase-app-compat.js");
-// importScripts("https://www.gstatic.com/firebasejs/9.17.1/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.17.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.17.1/firebase-messaging-compat.js");
+var functions = require ('firebase-functions');
 
 
-// firebase.initializeApp({
-//   apiKey: "AIzaSyBBwYwzl_pJPIJzzbz0H4rb2YOleOtRFuI",
-//   authDomain: "chessah-notify-pwa.firebaseapp.com",
-//   projectId: "chessah-notify-pwa",
-//   storageBucket: "chessah-notify-pwa.appspot.com",
-//   messagingSenderId: "686268554593",
-//   appId: "1:686268554593:web:268ca58bbd66530d95b920",
-//   measurementId: "G-F88QBGT2DD",
-//   vapidKey: "BAQmziT9Re-Vsav6kc1Vbl6K8jZ2WUazOt1GLnNe9BSNNElz8nExKAylgElzHOb2-v2ybgHJlnGhLID4QDqrM1I"
-// });
+firebase.initializeApp({
+  apiKey: "AIzaSyBBwYwzl_pJPIJzzbz0H4rb2YOleOtRFuI",
+  authDomain: "chessah-notify-pwa.firebaseapp.com",
+  projectId: "chessah-notify-pwa",
+  storageBucket: "chessah-notify-pwa.appspot.com",
+  messagingSenderId: "686268554593",
+  appId: "1:686268554593:web:268ca58bbd66530d95b920",
+  measurementId: "G-F88QBGT2DD",
+  vapidKey: "BAQmziT9Re-Vsav6kc1Vbl6K8jZ2WUazOt1GLnNe9BSNNElz8nExKAylgElzHOb2-v2ybgHJlnGhLID4QDqrM1I"
+});
 
-// const messaging = firebase.messaging();
+const messaging = firebase.messaging();
+
+export const sendNotificationOnObjectAdd = functions.firestore
+  .document('availability/{objectId}')
+  .onCreate(async (snapshot, context) => {
+    const objectData = snapshot.data();
+    const payload = {
+      notification: {
+        title: 'New Object Added',
+        body: `Object ID: ${context.params.objectId}`,
+      },
+    };
+
+    const tokens = []; // Add recipient tokens here
+
+    try {
+      await admin.messaging().sendToDevice(tokens, payload);
+      console.log('Notification sent successfully.');
+    } catch (error) {
+      console.error('Error sending notification:', error);
+    }
+  });
